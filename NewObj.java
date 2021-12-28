75d431ea4e30fc4c0dfcaf8d8b7769916d94f93d
santoshjc <50149373+santoshjc@users.noreply.github.com>
2021-12-29 00:04:55 +0530

    Add files via upload
    object crated and then assigned that object with null

---
+++ NewObj.java
@@ -0,0 +1,13 @@
+package corejava;
+
+public class NewObj {
+	
+	public static void main(String[] args) {
+		NewObj nw = new NewObj();
+		System.out.println(nw);
+		nw= null;
+		System.out.println(nw);
+		
+	}
+
+}
